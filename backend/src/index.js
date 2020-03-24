1e49ab05b29d50902e4d3767fb3feedae1449f36
JosimarPintoCamilo <48796118+JosimarPintoCamilo@users.noreply.github.com>
2020-03-24 18:01:59 -0300

    2.0.3
    Final do dia

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,12 @@
 const express = require('express'); //estou importando o modulo express para dentro do modulo express
+const cors = require('cors');
 //essa variavel tem todas as funcionaldades do express disponiveis para mim
 const routes = require('./routes');
+
 //criar a aplicação
 const app = express();
 
+app.use(cors());
 //para utilizar/entender json no corpo das requisições
 app.use(express.json());
 app.use(routes);
