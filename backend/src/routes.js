bc99caae527469c82f7e460a694b5255650a9549
JosimarPintoCamilo <48796118+JosimarPintoCamilo@users.noreply.github.com>
2020-03-24 17:30:27 -0300

    2.0.2
    terceira versao

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,26 +1,20 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.get('/ongs', async (request, response) => {
-    const ongs = await connection('ongs').select('*');
+routes.get('/ongs', OngController.list);
 
-    response.json(ongs);
-});
+routes.get('/profile', ProfileController.index);
 
-routes.post('/ongs', async (request, response) => {
-    const data = request.body;
-    const {name, email, whatsapp, city, uf} = request.body;
-    //console.log(data);
-    const id = crypto.randomBytes(4).toString('HEX');
+routes.post('/ongs', OngController.create);
+routes.post('/session', SessionController.create);
 
-    await connection('ongs').insert({
-        id, name, email,whatsapp, city, uf        
-    })
-
-    return response.json({ id });
-});
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.list);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
